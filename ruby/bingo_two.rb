1d6c2c4c0488c2a4f01d6bf34eef5ea33821a6ee
Ambreen Hasan <ambodumbo@gmail.com>
2015-02-17 20:12:55 -0800

    added logic for drawing and inserting matches

--- ruby/bingo_two.rb
+++ ruby/bingo_two.rb
@@ -19,10 +19,39 @@ class BingoBoard
       )}
   end
 
-  def draw
+  def player
   end
 
-  def player
+  def draw
+    @bingo_header = ["B", "I", "N", "G", "O"]
+
+    @bingo_header.length.times do |i|
+        case @bingo_header[i]
+        when "B"
+          @number = rand(1..15)
+        when "I"
+          @number = rand(16..30)
+        when "N"
+          @number = rand(31..45)
+        when "G"
+          @number = rand(46..60)
+        when "O"
+          @number = rand(61..75)
+        end
+      @letter_index = rand(0..4)
+      puts "#{@bingo_header[i]} #{@number}"
+      insert_match(i, @number)
+    end
+  end
+
+  def insert_match(drawn_letter, drawn_number)
+     @bingo = @bingo_board.map do |row|
+      if row[drawn_letter] == drawn_number
+        row[drawn_letter] = "X"
+      else
+        row[drawn_letter]
+      end
+    end
   end
 
   def check_horizontal
@@ -35,10 +64,13 @@ class BingoBoard
   end
 
   def to_s
-    @bingo_board
+    @bingo_board.each {|row| p row}
   end
 end
 
 board = BingoBoard.new
 
-board
+# p board.to_s
+
+board.draw
+board.to_s
